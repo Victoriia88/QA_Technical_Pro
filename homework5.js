ef0f6c58807b90f5149be1a4aaa091e21524b2f6
Victoriia Ulynets <123025408+Victoriia88@users.noreply.github.com>
2025-02-11 15:32:35 +0200

    update  hw7.1

--- homework5.js
+++ homework5.js
@@ -1,28 +1,46 @@
 let services = {
-  "стрижка": "60 грн",
-  "гоління": "80 грн",
-  "Миття голови": "100 грн",
+  'стрижка': '60 грн',
+  'гоління': '80 грн',
+  'Миття голови': '100 грн',
+
+  add(service, price) {
+    this[service] = price;
+  },
+
+  price() {
+    let total = 0;
+    for (let cost of Object.values(this)) {
+      if (typeof cost === 'string' && cost.includes('грн')) {
+        total += parseInt(cost);
+      }
+    }
+    return total + ' грн';
+  },
+
+  minPrice() {
+    let prices = [];
+    for (let cost of Object.values(this)) {
+      if (typeof cost === 'string' && cost.includes('грн')) {
+        prices.push(parseInt(cost));
+      }
+    }
+    return Math.min(...prices) + ' грн';
+  },
+
+  maxPrice() {
+    let prices = [];
+    for (let cost of Object.values(this)) {
+      if (typeof cost === 'string' && cost.includes('грн')) {
+        prices.push(parseInt(cost));
+      }
+    }
+    return Math.max(...prices) + ' грн';
+  }
 };
 
-services["Розбити скло"] = "200 грн";
-function price() {
-  let total = 0;
-  for (let cost of Object.values(services)) {
-    total += parseInt(cost);
-  }
-  return total + " грн";
-}
+services.add('Розбити скло', '200 грн');
+services.add('Фарбування', '600 грн');
 
-function minPrice() {
-  let prices = Object.values(services).map((value) => parseInt(value));
-  return Math.min(...prices) + " грн";
-}
-
-function maxPrice() {
-  let prices = Object.values(services).map((value) => parseInt(value));
-  return Math.max(...prices) + " грн";
-}
-
-console.log("Загальна вартість:", price());
-console.log("Мінімальна ціна:", minPrice());
-console.log("Максимальна ціна:", maxPrice());
+console.log('Загальна вартість:', services.price());
+console.log('Мінімальна ціна:', services.minPrice());
+console.log('Максимальна ціна:', services.maxPrice());
