21c085920f52dcfb9f973712142c684a4a65ed31
Victoriia Ulynets <123025408+Victoriia88@users.noreply.github.com>
2025-02-23 18:43:22 +0200

    create hw10.2

--- homework5.js
+++ homework5.js
@@ -1,46 +1,4 @@
-let services = {
-  'стрижка': '60 грн',
-  'гоління': '80 грн',
-  'Миття голови': '100 грн',
+const text = "Wonderful Joyful Happiness Time Task Apple";
+const result = text.match(/\b[^Aa\s]{6,}\b/g);
 
-  add(service, price) {
-    this[service] = price;
-  },
-
-  price() {
-    let total = 0;
-    for (let cost of Object.values(this)) {
-      if (typeof cost === 'string' && cost.includes('грн')) {
-        total += parseInt(cost);
-      }
-    }
-    return total + ' грн';
-  },
-
-  minPrice() {
-    let prices = [];
-    for (let cost of Object.values(this)) {
-      if (typeof cost === 'string' && cost.includes('грн')) {
-        prices.push(parseInt(cost));
-      }
-    }
-    return Math.min(...prices) + ' грн';
-  },
-
-  maxPrice() {
-    let prices = [];
-    for (let cost of Object.values(this)) {
-      if (typeof cost === 'string' && cost.includes('грн')) {
-        prices.push(parseInt(cost));
-      }
-    }
-    return Math.max(...prices) + ' грн';
-  }
-};
-
-services.add('Розбити скло', '200 грн');
-services.add('Фарбування', '600 грн');
-
-console.log('Загальна вартість:', services.price());
-console.log('Мінімальна ціна:', services.minPrice());
-console.log('Максимальна ціна:', services.maxPrice());
+console.log(result);
